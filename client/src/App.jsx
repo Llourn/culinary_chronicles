ad9b9d011499f48fecc5013088d5b7b7a4376abc
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-19 20:55:18 -0300

    added shoelace

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,18 @@ import {
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
+
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav.jsx";
+
+setBasePath(
+  "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.4.0/dist/"
+);
 
 const httpLink = createHttpLink({
   uri: "/graphql",
