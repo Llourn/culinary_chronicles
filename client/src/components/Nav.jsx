51a850503f4910ab8d12aec2a712dd6287bca723
justin connors <justinconnors@hotmail.ca>
2023-06-21 20:59:30 -0300

    nav-bar completed for now, will need to add links to login/sign-up

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SlTab,
   SlTabGroup,
@@ -8,23 +8,47 @@ import {
 import styles from "./Nav.module.css";
 
 const Nav = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log("search submitted: ", searchValue);
+  };
+
   return (
     <SlTabGroup className={styles.group}>
-      <SlTab slot="nav" panel="culinary" white>
-        Culinary Chronicles
+      <SlTab slot="nav" panel="culinary">
+        <a href="#" style={{ textDecoration: "none", color: "white" }}>
+          Culinary Chronicles
+        </a>
       </SlTab>
+      {/* Will Link to Profile/Login/Sign-up When completed */}
       <SlTab slot="nav" panel="profile">
         🤦‍♀️
       </SlTab>
       <SlInput
+        type="search"
         size="small"
         pill
         slot="nav"
         panel="search"
-        placeholder="search"
+        placeholder="Search"
+        value={searchValue}
+        onInput={handleSearchChange}
+        onSubmit={handleSearchSubmit}
         style={{ marginTop: "0.75rem" }}
       >
-        <SlIcon name="search" slot="prefix"></SlIcon>
+        {/* Click the Icon to Search */}
+        <SlIcon
+          name="search"
+          slot="suffix"
+          onClick={handleSearchSubmit}
+          style={{ cursor: "pointer" }}
+        ></SlIcon>
       </SlInput>
     </SlTabGroup>
   );
