4c14d38aef24d39c1aa0843a0fce29d2f08796cd
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-22 20:33:23 -0300

    a few small changes

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { SlButton} from '@shoelace-style/shoelace/dist/react';
+import { SlButton } from "@shoelace-style/shoelace/dist/react";
 
-const Button = (props) =>{
+const Button = ({
+  variant = "neutral",
+  style,
+  handleClick,
+  size = "medium",
+  children,
+}) => {
   return (
     <>
-      <SlButton  variant={props.variant} style={props.style} onClick={props.handleClick} size={props.size} href={props.href} outline> {props.children} </SlButton>
-    </> 
+      <SlButton
+        variant={variant}
+        style={style}
+        onClick={handleClick}
+        size={size}
+        outline
+      >
+        {children}
+      </SlButton>
+    </>
   );
 };
 
-export default Button ;
-
-// const Button = ({btnLabel, variant, style, size, onClick, href}) =>{
-//   return (
-//     <>
-//       <SlButton  variant={variant} style={style} onClick={onClick} size={size} href={href} outline> {btnLabel}</SlButton>
-//     </> 
-//   );
-// };
-// function Button(props){
-//   const myClass = `button ${props.type}`
-//   return(
-//   <button className={myClass} onClick={props.handleClick}>{props.children}</button>
-//   )
-//   }
-
+export default Button;
--- client/src/pages/NoMatch.jsx
+++ client/src/pages/NoMatch.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import Button from "../components/Button";
-
-const homeBtn= {variant: "neutral" ,
-style: {margin: "2 rem"},
-onClick: "handleclick and go to Home page",
-size: "medium",
-href: "",
-btnLabel:"HOME"
-};
+import { useNavigate } from "react-router-dom";
 
 const NoMatch = () => {
-  //console.log("not found");
+  const navigate = useNavigate();
+
   return (
-    <div  className="container">
-      <h1 >This page does not exist</h1>
-      <Button style={homeBtn.style} 
-             size={homeBtn.size} variant={homeBtn.variant} 
-             onclick={homeBtn.handleClick} href={homeBtn.href}>HOME</Button>
+    <div className="container">
+      <h1>This page does not exist</h1>
+      <Button handleClick={() => navigate("/")}>HOME</Button>
     </div>
   );
 };
