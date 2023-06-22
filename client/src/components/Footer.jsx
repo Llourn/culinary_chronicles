a0eb709e40174111ba42809576e6f1d5af814fd4
justin connors <justinconnors@hotmail.ca>
2023-06-21 21:35:01 -0300

    footer has github icon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import { FaGithub } from "react-icons/fa"
 
 const Footer = () => {
     const year = new Date().getFullYear();
     return (
         <footer>
-            {`©️ Culinary Chronicles™️ ${year}`} <a target='_blank' href='https://github.com/Llourn/culinary_chronicles'>Repo</a>
+            {`©️ Culinary Chronicles™️ ${year}`} <a target='_blank' href='https://github.com/Llourn/culinary_chronicles'><FaGithub /></a>
         </footer>
     )
 };
 
 export default Footer;
+
