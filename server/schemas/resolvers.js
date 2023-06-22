01be50ce83b1d1f3268fe0a56a7f4c7a6721478a
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-22 19:31:58 -0300

    updated model and resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,13 +6,7 @@ const resolvers = {
   Query: {
     user: async (parent, args, context) => {
       if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: "orders.products",
-          populate: "category",
-        });
-
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
-
+        const user = await User.findById(context.user._id);
         return user;
       }
 
