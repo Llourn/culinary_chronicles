abb6fef342f267877f1158b79c3b309b81efc6c3
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-23 14:47:55 -0300

    updated user seed data and added recipe seed data

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 const User = require("./User");
 const Recipe = require("./Recipe");
 const LikedRecipe = require("./LikedRecipe");
-const UserRecipe = require("./UserRecipe");
 
-module.exports = { User, Recipe, LikedRecipe, UserRecipe };
+module.exports = { User, Recipe, LikedRecipe };
