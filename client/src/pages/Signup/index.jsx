5030b3cac2faf4adb4db7b4399555f30ebfc90a5
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-24 13:52:14 -0300

    updated signup page with new components and styling

--- client/src/pages/Signup/index.jsx
+++ client/src/pages/Signup/index.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import Auth from "../../utils/auth";
 import { ADD_USER } from "../../utils/mutations";
+import { SlButton, SlInput } from "@shoelace-style/shoelace/dist/react";
+
+import styles from "./Signup.module.css";
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -31,55 +34,61 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
-
-      <h2>Signup</h2>
+    <div className={styles.formContainer}>
+      <h2>Sign Up</h2>
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
+        <SlInput
+          label="First Name"
+          name="firstName"
+          type="firstName"
+          id="firstName"
+          required
+          onSlInput={handleChange}
+        />
+        <br />
+        <SlInput
+          label="Last Name"
+          name="lastName"
+          type="lastName"
+          id="lastName"
+          required
+          onSlInput={handleChange}
+        />
+        <br />
+        <SlInput
+          label="Email"
+          placeholder="krillin@baldbychoice.dbz"
+          name="email"
+          type="email"
+          id="email"
+          required
+          onSlInput={handleChange}
+        />
+        <br />
+        <SlInput
+          className="input"
+          label="Password"
+          name="password"
+          type="password"
+          passwordToggle
+          clearable
+          onSlInput={handleChange}
+          required
+          defaultValue=""
+          pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+          helpText="Min. 8 characters, at least 1 uppercase, lowercase, number and special character"
+        />
+        <br />
+        <SlButton type="submit" variant="primary" outline>
+          Sign up
+        </SlButton>{" "}
+        <SlButton type="reset" variant="warning" outline>
+          Reset
+        </SlButton>
       </form>
+      <p className={styles.loginCTA}>
+        Already have an account? <Link to="/login">Log in.</Link>
+      </p>
     </div>
   );
 }
