3339912091f09bf473a4ba826fabb2899af40459
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-24 12:10:08 -0300

    removed custom button component and updated pages using button

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import Button from "../components/Button";
+
 const Test = () => {
   return (
-    <div className="container">
-      <Button>Primary</Button>
-      <Button variant="success">Success</Button>
-      <Button variant="danger">danger</Button>
-      <Button variant="warning">warning</Button>
+    <div>
+      <h1>Test</h1>
     </div>
   );
 };
