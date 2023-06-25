afd5ab0f08b8952f6294cacf5ab1bfda09ee2473
justin connors <justinconnors@hotmail.ca>
2023-06-25 13:07:12 -0300

    card layout completed for now, need to add modal eventually

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -14,13 +14,26 @@ const Card = (props) => {
         src="https://images.unsplash.com/photo-1559209172-0ff8f6d49ff7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80"
         alt="Card image"
       />
-      <strong>{props.title}</strong>
+      <strong>{props.name}Placeholder</strong>
+      <br />
+      <small>{`${props.firstName} ${props.lastName}`}</small>
       <br />
       {props.description}
+      some description goes here this is just a placeholder dont worry about
+      this just taking space to test
       <br />
-      <small>{props.createdAt}</small>
+      <small>{props.createdAt}Created at placeholder 420:69:10</small>
       <div slot="footer">
-        <SlButton variant="dark">SEE RECIPE</SlButton>
+        <SlButton variant="dark" onClick={props.viewRecipe}>
+          SEE RECIPE
+        </SlButton>
+        <p>Total likes: 69{props.likes}</p>
+        {props.likes}
+        <SlRating
+          label="Rating"
+          getSymbol={() => '<sl-icon name="hand-thumbs-up-fill"></sl-icon>'}
+          max={1}
+        ></SlRating>
       </div>
     </SlCard>
   );
