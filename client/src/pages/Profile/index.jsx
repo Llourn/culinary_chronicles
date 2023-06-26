16b36447ee797fa96d525d2fc67efb433c307d6f
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-26 19:09:53 -0300

    added avatar and banner div

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { SlAvatar } from "@shoelace-style/shoelace/dist/react";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <div className={styles.banner}></div>
+      <SlAvatar label="User avatar" />
+    </div>
+  );
 };
 
 export default Profile;
