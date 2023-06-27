6be621bdc251bc2dc3c27c780cf1ef85247a1da9
“anar <sehar.uzair23@gmail.com>
2023-06-26 22:42:18 -0300

    search recipie is working and initial readme added

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # culinary_chronicles
+
+
+### Contents
+
+1. [Discription](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Screenshots](#Screenshots)
+5. [Heroku link](#Herokulink)
+6. [Cradits](#Cradits)
+
+## Description 
+
+
+
+## Installation
+
+
+
+## Usage
+
+
+## Heroku link
+
+
+
+## Screenshot
+Following is the screenshots of my application:
+![this fullfill all the requirements](.png)
+![this fullfill all the requirements](.png)
+![this fullfill all the requirements](.png)
+
+## Wishlist
+
+
+
+## Cradits
+Contributors:
+[Sehar Uzair](https://github.com/calligraphist)
+
+
+
+---
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved
--- client/src/pages/Search/index.jsx
+++ client/src/pages/Search/index.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "../../components/Card";
+import { useQuery } from "@apollo/client";
+import { QUERY_ALL_RECIPES} from "../../utils/queries"
+import styles from "./Search.module.css";
+import { SlIcon, SlInput, SlButton } from "@shoelace-style/shoelace/dist/react";
+import { SlSpinner } from '@shoelace-style/shoelace/dist/react';
 
 const Search = () => {
-  return <div>Search</div>;
+  const [searchInput, setSearchInput] = useState('');
+  const [searchSubmitted, setSearchSubmitted] = useState('');
+  const {loading, data} = useQuery( QUERY_ALL_RECIPES,{
+         variables:{name: searchSubmitted},
+         skip: !searchSubmitted
+      });
+      console.log(data);
+const searchRecipies = data?.recipes || []
+console.log(searchRecipies);
+   const handleSubmit = (event) =>{
+    event.preventDefault()
+    setSearchSubmitted(searchInput) 
+    setSearchInput("")
+   };
+  
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setSearchInput(value)
+  };
+
+  return (
+    <>
+      <div className={styles.text}>
+        <SlButton variant="text" size="small">
+          BREAKFAST
+        </SlButton>
+        <SlButton variant="text" size="small">
+          DINNER
+        </SlButton>
+        <SlButton variant="text" size="small">
+          DRINKS
+        </SlButton>
+        <br />
+        <SlButton variant="text" size="small">
+          LUNCH
+        </SlButton>
+        <SlButton variant="text" size="small">
+          DESSERT
+        </SlButton>
+      </div>
+      <SlInput
+        className={styles.labelOnLeft}
+        label="SEARCH RECIPIE"
+        placeholder="Type Recipie Name"
+        size="small"
+        value={searchInput}
+        clearable
+         onSlInput={handleChange}
+      ></SlInput>
+      <div className={styles.onRight}>
+        <SlButton variant="primary" onClick={handleSubmit} type="submit" outline>
+          SEARCH
+        </SlButton>
+      </div>
+
+      {searchRecipies.map((recipie) => {
+      return (<div className="container">
+         <Card
+            name={recipie?.name}
+            firstName = {recipie?.author.firstName}
+              lastName = {recipie?.author.lastName}
+             description = { recipie?.description}
+              createdAt={recipie?.createdAt}
+              likes ={recipie?.likes}
+              ingredients={recipie?.ingredients}
+              directions = {recipie?.directions}
+            />
+            </div>
+            )})}
+            
+              {/* <div onClick={handleSubmit}>Loading...
+              <SlSpinner />
+              </div>  */}
+    </>
+  );
 };
 
 export default Search;
