3aad658426d218ceb949428676c935aca0fe5ee2
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-06-28 14:30:31 -0300

    fixed user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,14 +1,15 @@
 import { gql } from "@apollo/client";
 
 export const QUERY_USER = gql`
-  {
+  query Query {
     user {
+      _id
       firstName
       lastName
       email
-      bannerUrl
       bio
       profilePicUrl
+      bannerUrl
     }
   }
 `;
